89117cfe29aa39340e2f55f6754f9bf5cee0ca60
Joyce Robbins <jtr13@users.noreply.github.com>
2020-11-26 17:49:43 -0500

    Update count_rmd.R

--- R/count_rmd.R
+++ R/count_rmd.R
@@ -16,7 +16,8 @@ count_rmds <- function() {
   rmds_in_yml <- rmds_in_yml[grepl(".[Rr]md", rmds_in_yml)]
   cat("dupes in _bookdown.yml:\n")
   dupes <- rmds_in_yml[duplicated(rmds_in_yml)]
-  if (length(dupes) > 0) print(dupes) else print("None")
+  if (length(dupes) > 0) print(dupes) else message("None")
   cat("Rmd files not in _bookdown.yml:\n")
-  print(rmd_files[!(rmd_files %in% rmds_in_yml)])
+  missing <- rmd_files[!(rmd_files %in% rmds_in_yml)]
+  if (length(missing) > 0) print(missing) else message("None")
 }
