1c7e1cc93dbd3a144dbc14bd016329107d09b0d8
Joyce Robbins <jtr13@users.noreply.github.com>
2020-11-26 13:24:19 -0500

    Update count_rmd.R

--- R/count_rmd.R
+++ R/count_rmd.R
@@ -5,21 +5,17 @@
 #' count_rmds()
 #'
 #' @export
-
 count_rmds <- function() {
   rmd_files <- list.files()
   rmd_files <- rmd_files[grepl(".[Rr]md$", rmd_files)]
   if(!file.exists("_bookdown.yml")) stop ("Can't find a _bookdown.yml file to analyze.")
-
   rmds_in_yml <- readLines("_bookdown.yml")
   rmds_in_yml <- rmds_in_yml  %>% stringr::str_remove_all("[',\\]]") %>%
     stringr::str_remove_all("#.*$") %>%
     stringr::str_trim()
   rmds_in_yml <- rmds_in_yml[grepl(".[Rr]md", rmds_in_yml)]
-
   cat("dupes in _bookdown.yml:\n")
-  rmds_in_yml[duplicated(rmds_in_yml)]
-
-  cat("Rmd files not in _bookdown.yml\n:")
-  rmd_files[!(rmd_files %in% rmds_in_yml)]
+  print(rmds_in_yml[duplicated(rmds_in_yml)])
+  cat("Rmd files not in _bookdown.yml:\n")
+  print(rmd_files[!(rmd_files %in% rmds_in_yml)])
 }
