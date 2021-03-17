342ff2a87ec883ee96110624e48ae3cf25d3f96c
Joyce Robbins <jtr13@columbia.edu>
2021-03-17 15:32:12 -0400

    add trevor.R

---
+++ R/trevor.R
@@ -0,0 +1,25 @@
+# Login to https://trevor.myschoolapp.com/
+# Click Julian, Schedule, View Full Schedule
+# Schedule Reports button, Choose Day Name (Grid)
+# Choose dates, set Show Dates to Yes
+# Click Export Data to Excel
+
+library(tidyverse)
+# may be skip = 3 instead
+df <- readxl::read_xls("~/Downloads/trevor.xls", skip = 2)
+colnames(df)[1] <- "Time"
+gather(df, value = "Subject", key = "Start Date", -Time) %>%
+  filter(!is.na(Subject)) %>%
+  mutate(Time = str_replace_all(Time, "AM", " AM"),
+         Time = str_replace_all(Time, "PM", " PM")) %>%
+  separate(col = "Time", into = c("Start Time", "End Time"),
+                  sep = " - \\r\\s?") %>%
+  mutate(`Start Date` = str_extract(`Start Date`, "\\d+/\\d+/\\d+")) %>%
+  mutate(`End Date` = `Start Date`) %>%
+  select(Subject, `Start Date`, `Start Time`, `End Date`, `End Time`) %>%
+  mutate(`All day event` = FALSE, Description = "", Location = "") %>%
+  filter(!(str_detect(Subject, "Lunch"))) %>%
+  filter(!(str_detect(Subject, "Homeroom"))) %>%
+  write_csv("~/Downloads/trevorgoogle.csv")
+
+# In Google Calendar, Click Settings, then Import & Export
